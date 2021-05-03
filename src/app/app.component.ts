cf0886104e1beb913a46485372d3cf612c5fbc7c
guruproschool <82388640+guruproschool@users.noreply.github.com>
2021-05-04 01:21:55 +0530

    fix: bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { User } from './_models/user';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { User } from "./_models/user";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(
-    private router: Router, 
-  ) {
-   }
-
-  title = 'proschool-new';
+  title = "proschool-new";
   user: User;
 
   ngOnInit() {
-    this.user = JSON.parse(localStorage.getItem('currentUser'));
-    // function storageChange(event) {
-    //   if (event.key === "logged_in" && event.newValue != null) {
-    //     return this.router.navigate(['/login']); // If you are using router
-    //   }
-    // } 
-    // window.addEventListener('storage', storageChange, false)
-    window.addEventListener('storage', (event) => {
-      if (event.storageArea == localStorage) {
-        let token = localStorage.getItem('currentUser');
-        if(token == null || token == undefined) { // you can update this as per your key
-          return this.navigate()
+    this.user = JSON.parse(localStorage.getItem("currentUser"));
+    window.addEventListener(
+      "storage",
+      (event) => {
+        if (event.key === "currentUser") {
+          let token = localStorage.getItem("currentUser");
+          if (token == null || token == undefined) {
+            // you can update this as per your key
+            return this.navigate();
+          }
         }
-      }
-    }, false);
+      },
+      false
+    );
+    // window.addEventListener('storage', (event) => {
+    //   if (event.storageArea == localStorage) {
+    //     let token = localStorage.getItem('currentUser');
+    //     if(token == null || token == undefined) { // you can update this as per your key
+    //       return this.navigate()
+    //     }
+    //   }
+    // }, false);
     // if(localStorage.getItem('currentUser')) {
     //   if(this.user.role === 'admin') {
     //     this.router.navigate(['/main/main']);
@@ -46,9 +49,9 @@ export class AppComponent implements OnInit {
     //   console.log(JSON.parse(localStorage.getItem('currentUser')))
     // }
   }
-    
+
   navigate() {
-    window.location.reload()
-    this.router.navigate(['/']); // If you are using router
+    window.location.reload();
+    this.router.navigate(["/"]); // If you are using router
   }
 }
